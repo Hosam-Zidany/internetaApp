701e4c368f490717bcf476f8efca786462ba7805
Hosam-Zidany <107244822+Hosam-Zidany@users.noreply.github.com>
2024-04-28 21:09:07 +0300

    git add

---
+++ index.js
@@ -0,0 +1,24 @@
+ReactDOM.render(
+  <div className="container">
+    <nav><h1>Navbar-React</h1>
+    <div>
+      <ul id="navB">
+        <li><a className="act" href="index.html">Home</a></li>
+        <li><a href="index.html">Main</a></li>
+        <li><a href="index.html">About</a></li>
+        <li><a href="index.html">Contact</a></li>
+      </ul>
+    </div>
+    </nav>
+  </div>
+    , document.getElementById('root1') 
+);
+
+ReactDOM.render(
+    <div className="center">
+      <h1>This is a simple Navbar for pc view using React</h1>
+    </div>
+    , document.getElementById('root2') 
+);
+
+
